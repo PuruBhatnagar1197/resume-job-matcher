a397cbd1c07e8f689e95a7a76d406e9e2da7e8d8
Puru Bhatnagar <143126102+PuruBhatnagar1197@users.noreply.github.com>
2025-06-01 17:55:18 +0530

    Update resume_parser.py

--- utils/resume_parser.py
+++ utils/resume_parser.py
@@ -3,12 +3,7 @@ import re
 import subprocess
 import sys
 
-try:
-    nlp = spacy.load("en_core_web_sm")
-except OSError:
-    subprocess.check_call([sys.executable, "-m", "spacy", "download", "en_core_web_sm"])
-    nlp = spacy.load("en_core_web_sm")
-
+nlp = spacy.load("en_core_web_sm")
 
 def clean_text(text):
     text = text.replace('\r\n', '\n').replace('\r', '\n')
